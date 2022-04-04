8b49ae1cdfbad713126ebdddacbeed1a9725f3ee
ozumin <mizuo.n@gmail.com>
2022-04-04 22:09:49 +0900

    init & done using_stacks_to_arrange_views

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Mizuo Nagayama on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
